7f7166b7dca4d654335ff29a90a9a6646e38cc86
Stéphane HEURTAUX <SHEURTAUX@groupeisagri.com>
2022-11-15 20:25:38 +0100

    Mise en place de la ReactiveForm

--- demo-start-v12/src/app/customers/customer.component.ts
+++ demo-start-v12/src/app/customers/customer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { FormControl, FormGroup } from '@angular/forms';
 import { Customer } from './customer';
+
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +9,29 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm!: FormGroup;
   customer = new Customer();
 
-  constructor() { }
-
   ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true)
+    });
   }
 
-  save(customerForm: NgForm): void {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+/*       email: 'jack@torchwood.com',
+ */      sendCatalog: false
+    });
   }
 }
